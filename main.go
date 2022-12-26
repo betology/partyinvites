708691ba2bb23a1ae12fe6b8aa1377d420bcdd5b
betology <betology@gmail.com>
2022-12-25 22:53:28 -0600

    function loadTemplates added

--- main.go
+++ main.go
@@ -1,14 +1,48 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"html/template"
+)
 
-type Rsvp struct {
-	Name, Email, Phone string
-	WillAttend bool
-}
+/*
+	 type Rsvp struct {
+		Name, Email, Phone string
+		WillAttend         bool
+	}
 
 var responses = make([]*Rsvp, 0, 10)
+*/
+var templates = make(map[string]*template.Template, 3)
+
+/*
+func loadTemplates() {
+	templateNames := [5]string{"whelcome", "form", "thanks", "sorry", "list"}
+	for index, name := range templateNames {
+		t, err := template.ParseFiles("layout.html", name+".html")
+		if err == nil {
+			templates[name] = t
+			fmt.Println("Loaded template", index, name)
+		} else {
+			panic(err)
+		}
+	}
+}
+*/
+
+func loadTemplates() {
+	templateNames := [5]string{"welcome", "form", "thanks", "sorry", "list"}
+	for index, name := range templateNames {
+		t, err := template.ParseFiles("layout.html", name+".html")
+		if err == nil {
+			templates[name] = t
+			fmt.Println("Loaded template", index, name)
+		} else {
+			panic(err)
+		}
+	}
+}
 
 func main() {
-	fmt.Println("TODO: add some features")
+	loadTemplates()
 }
